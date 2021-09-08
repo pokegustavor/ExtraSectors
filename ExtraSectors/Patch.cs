bbe4d7c724b30667d5a6512b89689f9f68c7f59e
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-09-08 20:23:44 -0300

    Adicionar arquivos de projeto.

---
+++ ExtraSectors/Patch.cs
@@ -0,0 +1,78 @@
+﻿using HarmonyLib;
+
+namespace ExtraSectors
+{
+    [HarmonyPatch(typeof(PLGalaxy), "Setup")]
+    class Patch
+    {
+        static void Postfix(PLGalaxy __instance, PLRand ___m_RandomGenerator) 
+        {
+            for (int k = 0; k < 1500; k++)
+            {
+                PLSectorInfo randomSectorInfo = __instance.GetRandomSectorInfo(___m_RandomGenerator.Next());
+                if (randomSectorInfo.VisualIndication == ESectorVisualIndication.NONE && randomSectorInfo.MySPI.Faction != 4)
+                {
+                    randomSectorInfo.Name = "Grandyos Sys (" + randomSectorInfo.ID + ")";
+                    randomSectorInfo.VisualIndication = ESectorVisualIndication.CANYON;
+                    break;
+                }
+            }
+            for (int k = 0; k < 1500; k++)
+            {
+                PLSectorInfo randomSectorInfo = __instance.GetRandomSectorInfo(___m_RandomGenerator.Next());
+                if (randomSectorInfo.VisualIndication == ESectorVisualIndication.NONE && randomSectorInfo.MySPI.Faction != 4)
+                {
+                    randomSectorInfo.VisualIndication = ESectorVisualIndication.MOLTEN_ASTEROID;
+                    break;
+                }
+            }
+            for (int k = 0; k < 1500; k++)
+            {
+                PLSectorInfo randomSectorInfo = __instance.GetRandomSectorInfo(___m_RandomGenerator.Next());
+                if (randomSectorInfo.VisualIndication == ESectorVisualIndication.NONE && randomSectorInfo.MySPI.Faction == 4)
+                {
+                    randomSectorInfo.VisualIndication = ESectorVisualIndication.INFECTED_SPACE_CAVE;
+                    break;
+                }
+            }
+            for (int k = 0; k < 1500; k++)
+            {
+                PLSectorInfo randomSectorInfo = __instance.GetRandomSectorInfo(___m_RandomGenerator.Next());
+                if (randomSectorInfo.VisualIndication == ESectorVisualIndication.NONE && randomSectorInfo.MySPI.Faction != 4)
+                {
+                    randomSectorInfo.VisualIndication = ESectorVisualIndication.SPACE_JELLIES;
+                    break;
+                }
+            }
+            for (int k = 0; k < 1500; k++)
+            {
+                PLSectorInfo randomSectorInfo = __instance.GetRandomSectorInfo(___m_RandomGenerator.Next());
+                if (randomSectorInfo.VisualIndication == ESectorVisualIndication.NONE && randomSectorInfo.MySPI.Faction != 4)
+                {
+                    randomSectorInfo.VisualIndication = ESectorVisualIndication.STRANGE_OBJECT_RAD;
+                    break;
+                }
+            }
+            for (int k = 0; k < 1500; k++)
+            {
+                PLSectorInfo randomSectorInfo = __instance.GetRandomSectorInfo(___m_RandomGenerator.Next());
+                if (randomSectorInfo.VisualIndication == ESectorVisualIndication.NONE && randomSectorInfo.MySPI.Faction != 4)
+                {
+                    randomSectorInfo.Name = "Circulos Sys (" + randomSectorInfo.ID + ")";
+                    randomSectorInfo.VisualIndication = ESectorVisualIndication.GREY_PLAINS;
+                    break;
+                }
+            }
+            for (int k = 0; k < 1500; k++)
+            {
+                PLSectorInfo randomSectorInfo = __instance.GetRandomSectorInfo(___m_RandomGenerator.Next());
+                if (randomSectorInfo.VisualIndication == ESectorVisualIndication.NONE && randomSectorInfo.MySPI.Faction != 4)
+                {
+                    randomSectorInfo.Name = "Dynamos Sys (" + randomSectorInfo.ID + ")";
+                    randomSectorInfo.VisualIndication = ESectorVisualIndication.CANYON;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ExtraSectors/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace ExtraSectors
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0";
+
+        public override string Author => "pokegustavo";
+
+        public override string ShortDescription => "Re-adds some removed sectors";
+
+        public override string Name => "Extra Sectors";
+
+        public override string HarmonyIdentifier()
+        {
+            return "pokegustavo.ExtraSectors";
+        }
+    }
+}
